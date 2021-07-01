a019d340182c1c7c678259b0e32671a8e7e46af4
Hunter Hsu <80113hunterhsu@gmail.com>
2021-07-02 02:10:29 +0800

    tagSwitcher on index.html done
    all tags set, not prepared to be universal yet

--- js/tagSwitcher.js
+++ js/tagSwitcher.js
@@ -2,27 +2,47 @@
 //index.html
 var tagIndex = [
     //usage: tagIndex[tag title/tag class][num];
+    //tag class
+    [
+        "",  //0: title
+        "index_all",    //1: all categories
+        "index_lt",     //2: learning & technology
+        "index_dt",     //3: design & technology
+        "index_cs"      //4: computer science
+    ], 
     //tag title
     [
+        "", 
         "所有分類", 
         "學習與科技組", 
         "設計與科技組", 
         "資工系雙主修"
-    ], 
-    //tag class
-    [
-        "index_all" //all categories
-        "index_lt", //learning & technology
-        "index_dt", //design & technology
-        "index_cs"  //computer science
     ]
 ];
 
-function switcher(var tagOn, var tagOff) {
-    tagOff.style.display = "none";
-    tagOn.style.display = "inline";
+function itemCaller(tagNo) {
+    console.log("tagNo = " + tagNo);
+    var tagOff = document.getElementsByClassName("index_all");
+    var tagOn = document.getElementsByClassName(tagIndex[0][tagNo]);
+    for (var i = 0; i < tagOff.length; i++) {
+        tagOff[i].style.display = "none";
+    }
+    for (var i = 0; i < tagOn.length; i++) {
+        tagOn[i].style.display = "block";
+    }
 }
 
-function caller(var callOn, var callOff) {
-    
+function titleCaller(titleNo) {
+    var calledTitle = document.getElementById("index_title");
+    calledTitle.innerHTML = tagIndex[1][titleNo];
+    console.log("Tag: " + tagIndex[1][titleNo]);
 }
+
+function getTag(callNo) {
+    //get title with titleCaller
+    titleCaller(callNo);
+    
+    //get items of current tag
+    itemCaller(callNo);
+}
+
