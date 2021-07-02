adc5368832a764bd122f98f65544a00be6d6f54f
Hunter Hsu <80113hunterhsu@gmail.com>
2021-07-02 14:44:55 +0800

    tag btn color change added to tagSwitcher

--- js/tagSwitcher.js
+++ js/tagSwitcher.js
@@ -19,13 +19,6 @@ function getTag(pageNo, callNo) {
 var tagIndex = [
     //usage: tagIndex[tag title/tag class][num];
     //tag class
-    [
-        "",  //0: title
-        "index_all",    //1: all categories
-        "index_lt",     //2: learning & technology
-        "index_dt",     //3: design & technology
-        "index_cs"      //4: computer science
-    ], 
     //tag title
     [
         "", 
@@ -33,21 +26,39 @@ var tagIndex = [
         "學習與科技組", 
         "設計與科技組", 
         "資工系雙主修"
+    ], 
+    [
+        "",  //0: title
+        "index_all",    //1: all categories
+        "index_lt",     //2: learning & technology
+        "index_dt",     //3: design & technology
+        "index_cs"      //4: computer science
     ]
 ];
 function indexCaller(callNo) {
+    //get title
     var calledTitle = document.getElementById("index_title");
-    calledTitle.innerHTML = tagIndex[1][callNo];
-    console.log("Tag: " + tagIndex[1][callNo]);
-    
+    calledTitle.innerHTML = tagIndex[0][callNo];
+    console.log("Tag: " + tagIndex[0][callNo]);
+    //get tagged items
     var tagOff = document.getElementsByClassName("index_all");
-    var tagOn = document.getElementsByClassName(tagIndex[0][callNo]);
+    var tagOn = document.getElementsByClassName(tagIndex[1][callNo]);
     for (var i = 0; i < tagOff.length; i++) {
         tagOff[i].style.display = "none";
     }
     for (var i = 0; i < tagOn.length; i++) {
         tagOn[i].style.display = "block";
     }
+    //get pressed tag color
+    var removeTagColor = document.getElementsByClassName("indexTag");
+    for (var i = 0; i < removeTagColor.length; i++) {
+        removeTagColor[i].classList.remove('btn-primary');
+        removeTagColor[i].classList.add('btn-dark');
+    }
+    var addTagColor = "indexTagger" + callNo;
+    document.getElementById(addTagColor).classList.remove('btn-dark');
+    document.getElementById(addTagColor).classList.add('btn-primary');
+    //console.log("Tag Button Color Changed. ");
 }
 
 //1: LaserCut.html
