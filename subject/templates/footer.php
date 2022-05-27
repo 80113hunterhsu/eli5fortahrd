0b0c66028707eb89ba86f756792200d7cd02284d
Hunter Hsu <80113hunterhsu@gmail.com>
2022-05-28 01:46:45 +0800

    updated footer info

--- subject/templates/footer.php
+++ subject/templates/footer.php
@@ -1,16 +1,15 @@
 <footer class="footer text-center" style="background-color: #ffc107;">
     <div class="container">
-        <p class="text-center">
+        <p class="text-center hider">
             網站設計＆維護：<a href="mailto:80113hunterhsu@gmail.com?subject=科技系懶人包">許恆齊</a>
         </p>
         <hr>
         <p class="" style="text-align: center;">
-            科技系 數位學習方法與策略 專題<br>
+            科技系 系統分析與設計 專題<br>
             40871227H 陳致愷<br>
-            40871121H 陳昱綸<br>
-            40871204H 蘇柏霖<br>
             40871218H 許恆齊<br>
-            40871232H 黃家偉
+            40871204H 蘇柏霖<br>
+            40971901H 許嘉恩
         </p>
         <p class="text-muted small mb-0">Copyright &copy; <a href="https://www.tahrd.ntnu.edu.tw/" target="_blank">TAHRD 2021</a></p>
     </div>
